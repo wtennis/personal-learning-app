1b0f2be8c1b9fd7f39fea356d223d71e14dc7425
Whiting <whitinghtennis@gmail.com>
2021-09-23 12:38:25 -0700

    beefed up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,26 +15,30 @@ puts '🌱 🌱 🌱 Seeding 🌱 🌱 🌱'
 
 user1 = User.create(username: "maya", password: "floot", email: "maya@gmail.com")
 
-f1 = Folder.create(user_id: user1.id, name: 'Folder 1', emoji: '📁')
-f2 = Folder.create(user_id: user1.id, name: 'Folder 2', emoji: '📁')
-f3 = Folder.create(user_id: user1.id, name: 'Folder 3', emoji: '📁')
-f4 = Folder.create(user_id: user1.id, name: 'I am in folder 1', emoji: '💾')
-f5 = Folder.create(user_id: user1.id, name: 'I am also in folder 1', emoji: '💾')
-f6 = Folder.create(user_id: user1.id, name: 'I am in folder 2', emoji: '💾')
-f7 = Folder.create(user_id: user1.id, name: 'I am also in folder 2', emoji: '💾')
+f1 = Folder.create(user_id: user1.id, name: 'Folder 1: top-level', emoji: '📁')
+    fB = Folder.create(user_id: user1.id, name: 'B: in folder 1', emoji: '💾')
+    fB_nest = FolderContent.create(folder_id: f1.id, contentsable_id: fB.id, contentsable_type: fB.class.name)
+        fx = Folder.create(user_id: user1.id, name: 'x: in folder B', emoji: '💾')
+        fx_nest = FolderContent.create(folder_id: fB.id, contentsable_id: fx.id, contentsable_type: fx.class.name)
+    fC = Folder.create(user_id: user1.id, name: 'C: in folder 1', emoji: '💾')
+    fC_nest = FolderContent.create(folder_id: f1.id, contentsable_id: fC.id, contentsable_type: fC.class.name)
+    r1 = Resource.create(name: "Cool resource in folder 1!", emoji: "🌈", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9koaucRZZ03Qwpj6JiTMPWzcNFNtsxmnc3fyBmKPALTtnbhZBnNyvL133RgiDQhA3BdcG7WM&usqp=CAc")
+    r2 = Resource.create(name: "Another resource in folder 1!", emoji: "🍩", url: "https://static4.depositphotos.com/1000350/368/i/950/depositphotos_3686095-stock-photo-chocolate-doughnut.jpg")
+    r1_nest = FolderContent.create(folder_id: f1.id, contentsable_id: r1.id, contentsable_type: r1.class.name)
+    r2_nest = FolderContent.create(folder_id: f1.id, contentsable_id: r2.id, contentsable_type: r2.class.name)
 
 
+f2 = Folder.create(user_id: user1.id, name: 'Folder 2: top-level', emoji: '📁')
+    fD = Folder.create(user_id: user1.id, name: 'D: in folder 2', emoji: '💾')
+    fD_nest = FolderContent.create(folder_id: f2.id, contentsable_id: fD.id, contentsable_type: fD.class.name)
+    fE = Folder.create(user_id: user1.id, name: 'E: in folder 2', emoji: '💾')
+    fE_nest = FolderContent.create(folder_id: f2.id, contentsable_id: fE.id, contentsable_type: fE.class.name)
 
-r1 = Resource.create(name: "Cool resource in folder 1!", emoji: "🌈", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9koaucRZZ03Qwpj6JiTMPWzcNFNtsxmnc3fyBmKPALTtnbhZBnNyvL133RgiDQhA3BdcG7WM&usqp=CAc")
-r2 = Resource.create(name: "Another resource in folder 1!", emoji: "🍩", url: "https://static4.depositphotos.com/1000350/368/i/950/depositphotos_3686095-stock-photo-chocolate-doughnut.jpg")
+f3 = Folder.create(user_id: user1.id, name: 'Folder 3: top-level', emoji: '📁')
 
-nest1 = FolderContent.create(folder_id: f1.id, contentsable_id: f4.id, contentsable_type: f4.class.name)
-nest2 = FolderContent.create(folder_id: f1.id, contentsable_id: f5.id, contentsable_type: f5.class.name)
-nest3 = FolderContent.create(folder_id: f1.id, contentsable_id: r1.id, contentsable_type: r1.class.name)
-nest4 = FolderContent.create(folder_id: f1.id, contentsable_id: r2.id, contentsable_type: r2.class.name)
 
-nest5 = FolderContent.create(folder_id: f2.id, contentsable_id: f6.id, contentsable_type: f6.class.name)
-nest6 = FolderContent.create(folder_id: f2.id, contentsable_id: f7.id, contentsable_type: f7.class.name)
+    
+
 
 
 
