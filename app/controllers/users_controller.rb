0b5433d8bef5de4935b0ae3d8c5bda1428e00540
Whiting <whitinghtennis@gmail.com>
2021-09-22 18:40:24 -0700

    folder controller route and folder_contents controller route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,16 +13,16 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid? 
+            session[:user_id] = user.id
             render json: user, status: :created
         else
             render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
-
     private
 
     def user_params
-        params.permit(:username, :password, :password_confirmation)
+        params.permit(:username, :password, :password_confirmation, :email)
     end
 end
