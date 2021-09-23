9e87d93a6d985f8f6ce926f286e33b84e3f74e29
Whiting <whitinghtennis@gmail.com>
2021-09-23 14:56:24 -0700

    Typography no-wrap list item name

--- client/src/components/ExpandableItem.js
+++ client/src/components/ExpandableItem.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import LernList from './LernList';
@@ -34,7 +34,9 @@ function ExpandableItem({ folder }){
                 <ListItemIcon>
                     {folder.emoji}
                 </ListItemIcon>
-            <ListItemText primary={folder.name} /> 
+            <Typography noWrap> 
+                 {folder.name}
+            </Typography> 
             {open ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
         <Collapse in={open} timeout="auto" unmountOnExit>
