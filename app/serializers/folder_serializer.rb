ae951d856d92fce8a6ad830767a1911b72164c51
Whiting <whitinghtennis@gmail.com>
2021-09-27 20:26:19 -0700

    cleaned up home/login routes

--- app/serializers/folder_serializer.rb
+++ app/serializers/folder_serializer.rb
@@ -1,4 +1,4 @@
 class FolderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :emoji, :is_public, :has_contents
+  attributes :id, :name, :emoji, :is_public, :has_contents, :notes
   has_one :user
 end
