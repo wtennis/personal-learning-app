4b0f5e9eba2857d0ef5f419edaac5e8ebb761a52
Whiting <whitinghtennis@gmail.com>
2021-10-05 08:41:33 -0700

    all folders/resources in state

--- client/src/redux/reducers/dataReducer.js
+++ client/src/redux/reducers/dataReducer.js
@@ -7,16 +7,16 @@
             return state.concat(action.payload)
         case "data/getResources":
             return state.concat(action.payload)
-        case "folder/getFolderContents":
-            return state.map(folder => {
-                if (!folder.has_contents) {
-                    return folder
-                } 
-                else if (folder.id === action.payload.id){
-                    return {...folder, contents: action.payload.contents}
-                }
-                    return folder;
-            });
+        // case "folder/getFolderContents":
+        //     return state.map(folder => {
+        //         if (!folder.has_contents) {
+        //             return folder
+        //         } 
+        //         else if (folder.id === action.payload.id){
+        //             return {...folder, contents: action.payload.contents}
+        //         }
+        //             return folder;
+        //     });
         default:
             return state;
         }
