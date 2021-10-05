899fdf8978a287396b652a6ec40e8eb9194b67ce
Whiting <whitinghtennis@gmail.com>
2021-10-04 21:22:07 -0700

    controller fixes

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,8 +1,8 @@
 class FoldersController < ApplicationController
     
     def index
-        top_level_folders = Folder.top_levels.where(user_id: session[:user_id])
-        render json: top_level_folders
+        folders = Folder.all
+        render json: folders
     end
 
     def destroy
--- app/serializers/folder_serializer.rb
+++ app/serializers/folder_serializer.rb
@@ -1,4 +1,4 @@
 class FolderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :emoji, :is_public, :has_contents, :notes
+  attributes :id, :parent_folder_id, :name, :emoji, :is_public, :has_contents, :notes
   has_one :user
 end
